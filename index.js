eaa8589a7c70e4b1c7ed1799d8ba1e497e8ddaae
Mukarram Ishaq <mukarramishaq189@gmail.com>
2021-06-06 15:09:05 +0500

    setup Web Socket Server for realtime communication

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('message-server:server');
 const http = require('http');
 const express = require("express");
+const WebSocket = require("ws");
 
 /**
  * Get port from environment and store in the app.
@@ -13,7 +14,29 @@ const port = +(process.env.PORT || '3000');
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req, res) => res.json({ "hello": "hi" }))
+app.get('/', (req, res) => res.json({ "hello": "hi" }));
+
+
+/**
+ * Create a Websocket Server for realtime messaging
+ */
+const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket, request) => {
+    debug("A new connection established. yayyy!!!");
+});
+wss.on("error", (err) => {
+    debug("An error has occurred in WS Server: ", err);
+});
+wss.on("headers", (headers, request) => {
+    debug("Response Headers for new connection: ", headers.toString());
+});
+wss.on("listening", () => {
+    debug("WS Server is listening...");
+});
+wss.on("close", () => {
+    debug("WS Server is closed");
+});
 
 /**
  * Listen on provided port, on all network interfaces.
